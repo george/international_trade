2a25b36a65b1052d18527960c6756342eb52e4a2
George Anderson <george@benevolentcode.com>
2011-03-28 22:00:14 -0400

    currency converter is working!

--- lib/international_trade/currency_converter.rb
+++ lib/international_trade/currency_converter.rb
@@ -6,6 +6,7 @@ require 'xmlsimple'
 module InternationalTrade
   class CurrencyConverter
     attr_reader :cost, :exchange_data_file, :rates, :target_currency
+    attr_accessor :constituent_conversion_rates
 
     def initialize(exchange_data_file, target_currency)
       @target_currency = target_currency.upcase
@@ -14,44 +15,130 @@ module InternationalTrade
 
     # returns the amount expressed in :target_currency
     def convert(amount)
-      parse
-      
       @cost, from_currency = amount.split
-      @cost = Float(@cost)
-      
-      return BigDecimal(@cost.to_s, 10) if from_currency == self.target_currency
+      @cost = BigDecimal(@cost, 15)
 
-      BigDecimal((composite_conversion_rate(from_currency) * @cost).to_s, 10).banker_round
+      return BigDecimal(@cost.to_s, 15) if from_currency == self.target_currency
+      
+      parse_exchange_data_file
+
+      (composite_conversion_rate(from_currency) * @cost).banker_round
     end
 
     #######
     private
     #######
-
     def composite_conversion_rate(from_currency)
-      find_constituent_conversion_rates(from_currency).inject(1) do |mem, rate|
+      puts "=> from #{from_currency} (ultimately to: #{self.target_currency})"
+      puts "\tdomain:\n#{self.rates.map{|r| "\t\t#{r.from} => #{r.to}"}.join("\n")}"
+      # return BigDecimal('1.0', 15) if from_currency == self.target_currency
+
+      if exact_match = self.rates.detect{ |r| r.from == from_currency && r.to == self.target_currency }
+        puts "\t!!! exact_match: #{exact_match.conversion.to_s('F')}"
+        return exact_match.conversion
+      end
+      
+      sub_domain = self.rates.find_all{ |r| r.from == from_currency }
+      
+      puts "\t\tsub_domain:\n#{sub_domain.map{|r| "\t\t\t#{r.from} => #{r.to}"}.join("\n")}"
+      
+      sub_domain.each do |rate|
+        puts "\t>> looking at: #{rate.from} => #{rate.to}"
+        
+        self.rates.delete_if{ |r| r == rate || ( r.to == rate.from && r.from == rate.to ) } # no back-tracking
+        
+        if conversion = composite_conversion_rate(rate.to)
+          puts "\t!!! conversion match: #{rate.conversion.to_s('F')} (conversion: #{conversion.to_s('F')})"
+          return ( conversion * rate.conversion )
+          break
+        end
+      end
+    end
+
+    def yyy_composite_conversion_rate(from_currency)
+      return 1.0 if from_currency == self.target_currency
+
+# raise <<-EOS
+#
+# find_constituent_conversion_rates(from_currency): #{find_constituent_conversion_rates(from_currency).inspect}
+#
+# self.constituent_conversion_rates: #{self.constituent_conversion_rates.inspect}
+#
+# EOS
+
+      # raise find_constituent_conversion_rates(from_currency).inspect
+      find_constituent_conversion_rates(from_currency).inject(1.0) do |mem, rate|
         mem *= Float(rate.conversion)
       end
     end
-    
-    def find_constituent_conversion_rates(from_currency, constituent_conversion_rates = [])
-      puts "\n\n########################################\nfrom_currency: #{from_currency} (#{from_currency.class})"
-      potential_source_currencies = _potential_source_currencies(from_currency).reject do |src| 
+
+    def find_constituent_conversion_rates(from_currency)
+      puts "\n\n########################################\nfrom_currency: #{from_currency} (to: #{self.target_currency.inspect})"
+      potential_source_currencies = _potential_source_currencies(from_currency).reject do |src|
+        self.constituent_conversion_rates.map(&:from).include?(src.to)
+      end
+puts "potential_source_currencies:"
+potential_source_currencies.each do |curr|
+  puts "\t#{curr.inspect}"
+end
+
+      if potential_source_currencies.size > 1
+        if direct_match = potential_source_currencies.detect{ |c| c.to == self.target_currency }
+          puts "!!! pushing (direct_match): #{direct_match}"
+          self.constituent_conversion_rates << @rates.delete(direct_match).dup
+          puts "\tself.constituent_conversion_rates NOW is: #{self.constituent_conversion_rates.inspect}"
+        else
+          # recurse on potential_source_currencies
+          potential_source_currencies.reject{|s| s.to == from_currency}.each do |candidate|
+            @rates.delete(candidate)
+            @rates.delete_if{ |rate| rate.to == candidate.from && rate.from == candidate.to }
+            if find_constituent_conversion_rates((_candidate = candidate).dup.to)
+              puts "!!! pushing (after the fact): #{candidate}"
+              # self.constituent_conversion_rates << @rates.delete(_candidate).dup
+              self.constituent_conversion_rates << _candidate
+              puts "\tself.constituent_conversion_rates NOW is: #{self.constituent_conversion_rates.inspect}"
+              return self.constituent_conversion_rates
+            end
+          end
+        end
+      elsif potential_currency = potential_source_currencies.first
+        puts "!!! pushing (potential_currency): #{potential_currency}"
+        self.constituent_conversion_rates << @rates.delete(potential_currency).dup
+        puts "\tself.constituent_conversion_rates NOW is: #{self.constituent_conversion_rates.inspect}"
+        @rates.delete_if{ |rate| rate.to == potential_currency.from && rate.from == potential_currency.to }
+
+        # direct match on the target_currency?
+        if potential_currency.to == self.target_currency
+          return self.constituent_conversion_rates
+        else
+          return find_constituent_conversion_rates(potential_currency.to)
+        end
+      else
+        # do nothing (?)
+        puts "~~DOING NOTHING~~"
+      end
+
+# [OpenStruct.new(:conversion => 1_000_000_000_000)]
+    end
+
+    def xxx_find_constituent_conversion_rates(from_currency, constituent_conversion_rates = [])
+      puts "\n\n########################################\nfrom_currency: #{from_currency} (to: #{self.target_currency.inspect})"
+      potential_source_currencies = _potential_source_currencies(from_currency).reject do |src|
         constituent_conversion_rates.map(&:from).include?(src.to)
       end
 puts "potential_source_currencies:"
 potential_source_currencies.each do |curr|
-  puts "\t#{curr.inspect}" 
-end      
+  puts "\t#{curr.inspect}"
+end
       if potential_source_currencies.size > 1
-        
+
         # are any a direct match on the target_currency?
         if direct_match = potential_source_currencies.detect{ |c| c.to == self.target_currency }
 puts "PRE: @rates.size: #{@rates.size.inspect}"
 puts "PRE: @rates.size: #{constituent_conversion_rates.size.inspect}"
           constituent_conversion_rates << @rates.delete(direct_match)
-puts "POST: @rates.size: #{@rates.size.inspect}"          
-puts "POST: @rates.size: #{constituent_conversion_rates.size.inspect}"          
+puts "POST: @rates.size: #{@rates.size.inspect}"
+puts "POST: @rates.size: #{constituent_conversion_rates.size.inspect}"
           return constituent_conversion_rates
         else
           # recurse on potential_source_currencies
@@ -65,15 +152,15 @@ puts "POST: @rates.size: #{constituent_conversion_rates.size.inspect}"
       elsif potential_source_currencies.first # only 1 potential_source_currency
         potential_currency = potential_source_currencies.first
         constituent_conversion_rates << @rates.delete(potential_currency)
-        @rates.delete_if do |rate| 
+        @rates.delete_if do |rate|
           begin
             rate.to == potential_currency.from && rate.from == potential_currency.to
           rescue
             raise <<-EOS
-            
+
             rate: #{rate.inspect}
             potential_currency: #{potential_currency.inspect}
-            
+
             EOS
           end
         end
@@ -85,38 +172,40 @@ puts "POST: @rates.size: #{constituent_conversion_rates.size.inspect}"
         end
       end
     end
-    
+
     def conversion_rate_from_index(idx)
       @rates.detect{ |r| r.index == idx }.conversion
     end
 
-    def parse
-      @rates = _rates
+    def parse_exchange_data_file
+      @rates = _parse_exchange_data_file
       post_process_rates
     end
-    
+
     def post_process_rates
-      @rates = @rates.enum_for(:each_with_index).collect do |rate, idx|
+      @rates = @rates.collect do |rate|
+        # Float-ify conversion rates for cleaner code (later)
+        rate['conversion'] = BigDecimal(rate['conversion'], 15)
         # normalize currency casing
         %w(from to).each{ |x| rate[x] = rate[x].upcase}
         rate
       end
-      
+
       # convert the conversions to OpenStructs (mainly so we
       # can use symbol to proc later)
       @rates = rates.collect{ |rate| OpenStruct.new(rate)}
     end
-    
+
     def _potential_source_currencies(from_currency)
       @rates.find_all{|r| r.from == from_currency}
     end
-    
+
     def _potential_target_currencies(target_currency)
       @rates.find_all{|r| r.to == target_currency}
     end
-    
-    def _rates
-      @_rates ||= XmlSimple.xml_in(self.exchange_data_file, 'ForceArray' => false)['rate']
+
+    def _parse_exchange_data_file
+      @_parse_exchange_data_file ||= XmlSimple.xml_in(self.exchange_data_file, 'ForceArray' => false)['rate']
     end
   end
 end
--- spec/international_trade/currency_converter_spec.rb
+++ spec/international_trade/currency_converter_spec.rb
@@ -1,26 +1,65 @@
 require 'spec_helper'
 
 module InternationalTrade
+  # describe CurrencyConverter do
+  #   def convert(target_currency, amount)
+  #     # CurrencyConverter.new('data/SAMPLE_RATES.xml', target_currency).convert(amount).to_s('F')
+  #     CurrencyConverter.new('data/SAMPLE_RATES.xml', target_currency).convert(amount).to_s('F')
+  #   end
+  # 
+  #   describe "#convert" do
+  #     # no conversion
+  #     it "returns '101.67 USD' given '101.67 USD'" do
+  #       convert('USD', '101.67 USD').should == '101.67'
+  #     end
+  # 
+  #     # single-step conversion
+  #     it "returns '100.79 CAD' given '100 AUD'" do
+  #       convert('CAD', '100 AUD').should == '100.79'
+  #     end
+  # 
+  #     # two-step conversion
+  #     it "returns '101.7 USD' given '100.0 AUD'" do
+  #       convert('USD', '100 AUD').should == '101.7'
+  #     end
+  #   end
+  # end
+  
   describe CurrencyConverter do
-    def convert(target_currency, amount)
-      # CurrencyConverter.new('data/SAMPLE_RATES.xml', target_currency).convert(amount).to_s('F')
-      CurrencyConverter.new('data/SAMPLE_RATES.xml', target_currency).convert(amount).to_s('F')
+    def composite_conversion_rate(from_currency, target_currency)
+      converter = CurrencyConverter.new('data/RATES.xml', target_currency)
+      converter.send(:parse_exchange_data_file)
+      converter.constituent_conversion_rates = []
+      converter.send(:composite_conversion_rate, from_currency)
     end
-
-    describe "#convert" do
-      # no conversion
-      it "returns '101.67 USD' given '101.67 USD'" do
-        convert('USD', '101.67 USD').should == '101.67'
-      end
-
-      # single-step conversion
-      it "returns '100.79 CAD' given '100 AUD'" do
-        convert('CAD', '100 AUD').should == '100.79'
-      end
-
-      # two-step conversion
-      it "returns '101.7 USD' given '100.0 AUD'" do
-        convert('USD', '100 AUD').should == '101.7'
+    
+    describe "#composite_conversion_rate" do
+      CONVERSIONS = [ # ['AUD', 'AUD', '1.0'],
+                      ['AUD', 'CAD', '1.0079'],
+                      ['AUD', 'EUR', '0.7439'],
+                      ['AUD', 'USD', '1.0169711'],
+                      ['CAD', 'AUD', '0.9921'],
+                      # ['CAD', 'CAD', '1.0'],
+                      ['CAD', 'EUR', '0.73802319'],
+                      ['CAD', 'USD', '1.009'],
+                      ['EUR', 'AUD', '1.3442'],
+                      ['EUR', 'CAD', '1.35481918'],
+                      # ['EUR', 'EUR', '1.0'],
+                      ['EUR', 'USD', '1.36701255262'],
+                      ['USD', 'AUD', '0.98327031'],
+                      ['USD', 'CAD', '0.9911'],
+                      ['USD', 'EUR', '0.731454783609'],
+                      # ['USD', 'USD', '1.0']
+                    ]
+      # CONVERSIONS = [ ['EUR', 'CAD', '1.35481918'] ]
+      CONVERSIONS.each do |conversion|
+        it "returns '#{conversion[2]}' when converting from '#{conversion[0]}' to '#{conversion[1]}'" do
+          expected  = BigDecimal(conversion[2], 15)
+          converted = composite_conversion_rate(conversion[0], conversion[1])
+          
+          converted.class.should     == expected.class
+          converted.to_s('F').should == expected.to_s('F')
+        end
       end
     end
   end
